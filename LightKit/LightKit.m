5076081084c829a23064baa85462da7993630ed5
Maxime Mouchet <max@maxmouchet.com>
2015-05-04 21:44:17 +0200

    Initial Commit

---
+++ LightKit/LightKit.m
@@ -0,0 +1,13 @@
+//
+//  LightKit.m
+//  LightKit
+//
+//  Created by Max Mouchet on 04/05/15.
+//  Copyright (c) 2015 Maxime Mouchet. All rights reserved.
+//
+
+#import "LightKit.h"
+
+@implementation LightKit
+
+@end
