dfc7b66d5f6761a76d257791cc330c83005ae49a
Maxime Mouchet <max@maxmouchet.com>
2016-07-28 17:34:29 +0200

    Migrate to Framework target

---
+++ LightKit/LightKit.h
@@ -0,0 +1,19 @@
+//
+//  LightKit.h
+//  LightKit
+//
+//  Created by Max Mouchet on 28/07/16.
+//  Copyright © 2016 Maxime Mouchet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LightKit.
+FOUNDATION_EXPORT double LightKitVersionNumber;
+
+//! Project version string for LightKit.
+FOUNDATION_EXPORT const unsigned char LightKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightKit/PublicHeader.h>
+
+
